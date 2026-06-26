1a83efa987074066324de2aec1027609fa1085f6
nawal faazat zakia <kyeaselyana@gmail.com>
2026-06-26 11:17:14 +0700

    Update ChatContractTest.php

--- tests/Feature/ChatContractTest.php
+++ tests/Feature/ChatContractTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use App\Contract\ChatContract;
+use App\Contracts\ChatContract;
 use PHPUnit\Framework\MockObject\MockObject;
 
 class ChatContractTest extends TestCase
